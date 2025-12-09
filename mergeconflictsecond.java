3c096db30fbb74aa890aac547029519acb7b3370
Lathagsoftwareautomation30 <lathagsoftwareautomation30@gmail.com>
2025-12-09 14:35:18 +0530

    g'

---
+++ mergeconflictsecond.java
@@ -0,0 +1,15 @@
+package Help;
+
+public class mergeconflictsecond
+{
+	public static void main(String[] args)
+	{
+    int a,b;
+    a=10;
+    b=20;
+    int c=a +b;
+    System.out.println("output:"+c);
+    System.out.println("Head");
+	}
+
+}
